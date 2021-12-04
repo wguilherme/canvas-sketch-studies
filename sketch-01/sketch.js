c957ea2f0516031b38251421bcee4bd954a14242
W. Guilherme <ravok123@gmail.com>
2021-12-04 19:41:44 -0300

    [sketch-01] add dynamic measurements for sheet size

--- sketch-01/sketch.js
+++ sketch-01/sketch.js
@@ -1,24 +1,29 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [600, 600]
+  dimensions: [1080, 1080]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
 
-    const w = 60;
-    const h = 60;
-    const gap = 20;
+    const w = width * 0.10;
+    const h = height * 0.10;
+    const gap = width * 0.03;
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+
+    const off = width * 0.02;
     let x, y;
 
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        x = 100 + (w + gap) * i;
-        y = 100 + (h + gap) * j;
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
 
         context.beginPath()
         context.rect(x, y, w, h)
@@ -26,7 +31,7 @@ const sketch = () => {
 
         if (Math.random() > 0.5) {
           context.beginPath()
-          context.rect(x + 8, y + 8, w - 16, h - 16)
+          context.rect(x + off / 2, y + off / 2, w - off, h - off)
           context.stroke()
           // context.fillStyle = 'black'
           // context.fill()
